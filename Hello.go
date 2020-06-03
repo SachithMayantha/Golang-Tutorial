4ca73a6497fa5a5ab6cf3135cd0f9e6c1b12f17b
Sachith Mayantha Fernando <38182512+SachithMayantha@users.noreply.github.com>
2020-06-03 12:08:45 +0530

    Add leaning Golang file

---
+++ Hello.go
@@ -0,0 +1,13 @@
+//main package
+package main
+
+//import format package
+import ("fmt")
+
+//main method
+func main() {
+	var a,b int
+	a,b = 10,20 
+	result := a+b //this : replace to var
+	fmt.Print(result)
+}
