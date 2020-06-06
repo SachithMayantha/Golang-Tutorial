f07db1330981d346436bf17f5719b0ec3a1683ec
Sachith Mayantha Fernando <38182512+SachithMayantha@users.noreply.github.com>
2020-06-06 22:45:48 +0530

    If else condition

---
+++ if_else.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	num := 1
+
+	if num == 1 {
+		fmt.Println("One")
+	} else if num == 2 {
+		fmt.Println("Two")
+	} else {
+		fmt.Println("Three")
+	}
+
+}
